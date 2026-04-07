a257685ceaabedeb92bb210a5e756b8eda9ffda6
userbot20267-creator <userbot20267@gmail.com>
2026-04-07 16:19:58 +0300

    Update __init__.py

--- locales/__init__.py
+++ locales/__init__.py
@@ -1,1 +1,8 @@
+from locales.ar import AR_TEXTS
+from locales.en import EN_TEXTS
 
+
+def get_text(key, language="ar"):
+    if language == "en":
+        return EN_TEXTS.get(key, AR_TEXTS.get(key, key))
+    return AR_TEXTS.get(key, key)
